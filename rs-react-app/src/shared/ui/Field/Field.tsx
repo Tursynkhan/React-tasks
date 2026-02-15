50f93abee6621f6b1b69861d97f3136f122ff306
turynkhan <tursynkhan001@gmail.com>
2026-02-15 21:09:04 +0500

    feat: add Field ui component

---
+++ rs-react-app/src/shared/ui/Field/Field.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  type TextFieldProps,
+  FormControl,
+  FormLabel,
+  TextField,
+} from '@mui/material';
+
+interface FieldProps extends Omit<TextFieldProps, 'label'> {
+  label: string;
+}
+const Field = React.forwardRef<HTMLInputElement, FieldProps>((props, ref) => {
+  const { label, ...rest } = props;
+  return (
+    <FormControl>
+      <FormLabel htmlFor={rest.name}>{label}</FormLabel>
+      <TextField
+        ref={ref}
+        id={rest.name}
+        error={!!rest.error}
+        helperText={rest.helperText}
+        {...rest}
+      />
+    </FormControl>
+  );
+});
+
+Field.displayName = 'Field';
+export default Field;
