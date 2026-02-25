0a06f46df21a437664720b674f5fe5cb15e2a7ca
turynkhan <tursynkhan001@gmail.com>
2026-02-26 03:41:18 +0500

    feat: add Footer component to Layout

--- rs-react-app/src/widgets/Layout/Layout.tsx
+++ rs-react-app/src/widgets/Layout/Layout.tsx
@@ -1,16 +1,12 @@
 import { Box } from '@mui/material';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Box
       sx={{
         margin: '0 auto',
-        padding: {
-          xs: '10px 16px 20px 16px',
-          sm: '15px 24px 30px 24px',
-          md: '20px 40px 40px 40px',
-        },
         maxWidth: '1440px',
         minHeight: '100vh',
         position: 'relative',
@@ -18,6 +14,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     >
       <Header />
       {children}
+      <Footer />
     </Box>
   );
 }
